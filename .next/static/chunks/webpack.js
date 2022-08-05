c8d36a67858af0a3e9cfdb2829e95ce628b4c2a9
kelvin2020-git <79944997+kelvin2020-git@users.noreply.github.com>
2022-08-05 03:19:21 -0600

    fi commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e06ea007ea7d94cf"; }
+/******/ 		__webpack_require__.h = function() { return "ea4dda4fb19761d6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
